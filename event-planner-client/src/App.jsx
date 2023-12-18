bc58c22ea420a9346ded0c71ac2a3ad852cd0192
Marv0912 <marvin.fajardo.s@icloud.com>
2023-12-18 14:43:29 -0400

    create routing to main pages

--- event-planner-client/src/App.jsx
+++ event-planner-client/src/App.jsx
@@ -1,11 +1,29 @@
+import { CreateEvent } from './pages/CreateEvent'
+import { CategoryView } from './pages/CategoryView'
+import { EditEvent } from './pages/EditEvent'
+import { EventDetails } from './pages/EventDetails'
+import { Home } from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { Sidebar } from './components/Sidebar'
 import './App.css'
 
 function App() {
- 
+
 
   return (
     <>
-    
+    <Navbar />
+    <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<CreateEvent />} />
+        <Route path="/" element={<CategoryView />} />
+        <Route path="/" element={<EventDetails />} />
+        <Route path="/" element={<EditEvent />} />
+
+      </Routes>
+
     </>
   )
 }
