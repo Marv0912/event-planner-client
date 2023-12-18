d58422189ae55121ff6765fa825759f97c9eb448
Marv0912 <marvin.fajardo.s@icloud.com>
2023-12-18 16:19:57 -0400

    fix App file

--- event-planner-client/src/App.jsx
+++ event-planner-client/src/App.jsx
@@ -1,31 +1,32 @@
-import { CreateEvent } from './pages/CreateEvent'
-import { CategoryView } from './pages/CategoryView'
-import { EditEvent } from './pages/EditEvent'
-import { EventDetails } from './pages/EventDetails'
-import { Home } from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
-import { Navbar } from './components/Navbar'
-import { Sidebar } from './components/Sidebar'
+import CreateEvent from './pages/CreateEvent'
+import CategoryView from './pages/CategoryView'
+import EditEvent from './pages/EditEvent'
+import EventDetails from './pages/EventDetails'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 import './App.css'
+
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
-    <>
-    <Navbar />
-    <Sidebar />
+    <div>
+      <Navbar />
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<CreateEvent />} />
-        <Route path="/" element={<CategoryView />} />
-        <Route path="/" element={<EventDetails />} />
-        <Route path="/" element={<EditEvent />} />
+        <Route path="/create" element={<CreateEvent />} />
+        <Route path="/category" element={<CategoryView />} />
+        <Route path="/details" element={<EventDetails />} />
+        <Route path="/edit" element={<EditEvent />} />
 
       </Routes>
 
-    </>
-  )
+      </div>
+      )
 }
 
-export default App
+export default App;
