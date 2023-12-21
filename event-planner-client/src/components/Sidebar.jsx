4c6cf9e5a81569cd499696264cf2613c63502445
Marv0912 <marvin.fajardo.s@icloud.com>
2023-12-21 13:08:40 -0400

    sidebar takes 100vh

--- event-planner-client/src/components/Sidebar.jsx
+++ event-planner-client/src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
     return (
         <div className="border-end bg-light ">
-            <ul className="list-unstyled" >
+            <ul className="list-unstyled" style={{ height: '100vh'}}>
                 <li>
                     <Link to="/create" className="btn btn-primary btn-block mt-4 mb-4">Create Event</Link>
                 </li>
