2f5a4172eb4b4802cc5ba3ac63d3602156dec90f
Marv0912 <marvin.fajardo.s@icloud.com>
2023-12-21 13:08:10 -0400

    Move Link to only apply to event name

--- event-planner-client/src/components/EventCard.jsx
+++ event-planner-client/src/components/EventCard.jsx
@@ -7,22 +7,23 @@ const EventCard = ({ event, onDelete }) => {
     return (
         <div className='card mb-4'>
             <div className='card-body ' >
-            <Link to={`/${event.id}`} className='text-decoration-none text-dark'>
                     <div className='row align-items-center'>
                         <div className='col-md-9'>
+                <Link to={`/${event.id}`} className='text-decoration-none text-dark'>
                             <h3 className='card-title text-start'>{event.name}</h3>
-                        </div>
-                        <div className='col-md-3 text-end'>
-                            <button onClick={handleDeleteClick} className='btn btn-danger btn-sm'>X</button>
-                        </div>
-                    </div>
                 </Link>
-                    <p className='card-text text-start'>{event.startTime} - {event.endTime}</p>
-                    <p className='card-text text-start'>{event.category}</p>
-                    <p className='card-text text-start'>{event.location}</p>
-
+                        </div>
+                <div className='col-md-3 text-end'>
+                    <button onClick={handleDeleteClick} className='btn btn-danger btn-sm'>X</button>
+                </div>
             </div>
+
+            <p className='card-text text-start'>{event.startTime} - {event.endTime}</p>
+            <p className='card-text text-start'>{event.category}</p>
+            <p className='card-text text-start'>{event.location}</p>
+
         </div>
+        </div >
     );
 }
 
