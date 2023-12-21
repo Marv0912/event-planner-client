1462cbb3ff19a56cdd07ee8416ee15e99e4fc245
Marv0912 <marvin.fajardo.s@icloud.com>
2023-12-21 10:55:10 -0400

    move form state to App component and css styling

--- event-planner-client/src/pages/CreateEvent.jsx
+++ event-planner-client/src/pages/CreateEvent.jsx
@@ -1,24 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
-const CreateEvent = () => {
+const CreateEvent = ({ eventFormData, setEventFormData, onCreateEvent }) => {
 
-    // set one state for the whole data structure
-    const [form, setForm] = useState({
-        name: '',
-        category: 'Important',
-        date: '',
-        startTime: '',
-        endTime: '',
-        location: '',
-        description: '',
-    });
     const navigate = useNavigate();
     const handleInputChange = (e) => {
         // destructure to get name and value of field
         const { name, value } = e.target;
-        setForm((prevData) => ({
+        setEventFormData((prevData) => ({
             //spread operator to get data from useState
             ...prevData,
             [name]: value
@@ -27,92 +17,139 @@ const CreateEvent = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        axios.post('http://localhost:5005/events', form)
+        axios.post('http://localhost:5005/events', eventFormData)
             .then((response) => {
                 console.log('Event added: ', response.data);
+                onCreateEvent(response.data);
             })
             .catch((error) => {
                 console.error('Error: ', error);
             })
 
-            navigate('/')
+        navigate('/')
     }
 
 
     useEffect(() => {
-        console.log(form)
-    }, [form])
+        console.log(setEventFormData)
+    }, [setEventFormData])
     return (
-        <div>
+        <div className='container mt-4' >
             <Link to={"/"}>
-                <button>Back</button>
+                <button className='btn btn-secondary mb-3' >Back</button>
             </Link>
 
-            <h2>Create Event</h2>
+            <h2 className='mb-4' >Create Event</h2>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Name</label>
-                    <input
-                        type="text"
-                        name="name"
-                        onChange={handleInputChange}
-                        required
-                    />
+                <div className="mb-3 row">
+                    <label className="col-md-2 col-form-label fs-5">
+                        Name:
+                    </label>
+                    <div className="col-md-10">
+                        <input
+                            type="text"
+                            id="name"
+                            className="form-control"
+                            name="name"
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Date:</label>
-                    <input
-                        type="date"
-                        name="date"
-                        onChange={handleInputChange}
-                    />
+                <div className="mb-3 row">
+                    <label className="col-md-2 col-form-label fs-5">
+                        Date:
+                    </label>
+                    <div className="col-md-10">
+                        <input
+                            type="date"
+                            id="date"
+                            className="form-control"
+                            name="date"
+                            onChange={handleInputChange}
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Description:</label>
-                    <textarea
-                    name='description'
-                        onChange={handleInputChange}
-                    ></textarea>
+                <div className="mb-3 row">
+                    <label className="col-md-2 col-form-label fs-5">
+                        Description:
+                    </label>
+                    <div className="col-md-10">
+                        <textarea
+                            id="description"
+                            className="form-control"
+                            name="description"
+                            onChange={handleInputChange}
+                        ></textarea>
+                    </div>
                 </div>
-                <div>
-                    <label>Location</label>
-                    <input
-                        type="text"
-                        name="location"
-                        onChange={handleInputChange}
-                        required
-                    />
+                <div className="mb-3 row">
+                    <label className="col-md-2 col-form-label fs-5">
+                        Location:
+                    </label>
+                    <div className="col-md-10">
+                        <input
+                            type="text"
+                            id="location"
+                            className="form-control"
+                            name="location"
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Category:</label>
-                    <select name="category" onChange={handleInputChange}>
-                        <option value="Important">Important</option>
-                        <option value="Family">Family</option>
-                        <option value="Work">Work</option>
-                        <option value="Free-Time">Free-Time</option>
-                        <option value="Other">Other</option>
-                    </select>
+                <div className="mb-3 row">
+                    <label className="col-md-2 col-form-label fs-5">
+                        Category:
+                    </label>
+                    <div className="col-md-10">
+                        <select
+                            id="category"
+                            className="form-select"
+                            name="category"
+                            onChange={handleInputChange}
+                        >
+                            <option value="Important">Important</option>
+                            <option value="Family">Family</option>
+                            <option value="Work">Work</option>
+                            <option value="Free-Time">Free-Time</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </div>
                 </div>
-                <div>
-                    <label>Start Time:</label>
-                    <input
-                        //VERIFY TYPE
-                        type="time"
-                        name="startTime"
-                        onChange={handleInputChange}
-                    />
+                <div className="mb-3 row">
+                    <label className="col-md-2 col-form-label fs-5">
+                        Start Time:
+                    </label>
+                    <div className="col-md-10">
+                        <input
+                            type="time"
+                            id="startTime"
+                            className="form-control"
+                            name="startTime"
+                            onChange={handleInputChange}
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>End Time:</label>
-                    <input
-                        //VERIFY TYPE
-                        name="endTime"
-                        type="time"
-                        onChange={handleInputChange}
-                    />
+                <div className="mb-3 row">
+                    <label className="col-md-2 col-form-label fs-5">
+                        End Time:
+                    </label>
+                    <div className="col-md-10">
+                        <input
+                            type="time"
+                            id="endTime"
+                            className="form-control"
+                            name="endTime"
+                            onChange={handleInputChange}
+                        />
+                    </div>
                 </div>
-
-                <button type="submit">Add Event</button>
+                <div className="mb-3">
+                    <button type="submit" className="btn btn-primary">
+                        Add Event
+                    </button>
+                </div>
             </form>
         </div>
     );
