889e977ca6da9bcd6d53acf767baec1ed6eee4c8
Marv0912 <marvin.fajardo.s@icloud.com>
2023-12-21 13:10:09 -0400

    add search feature- in progress

--- event-planner-client/src/components/Navbar.jsx
+++ event-planner-client/src/components/Navbar.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-const Navbar = () => {
+const Navbar = ({ onSearch, clearSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+  const handleClearSearch = () => {
+    clearSearch();
+  }
   return (
-    <nav>
-        <ul>
-            <li>
-                <Link to="/"> Home </Link>
-            </li>
-            <li>
-                <Link to="/">Somewhere Else</Link>
-            </li>
-        </ul>
+    <nav className="navbar border" >
+      <Link to="/" className="navbar-brand px-3"> Home </Link>
+      <div>
+        <input
+          type="text"
+          placeholder="Search events"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="rounded-pill"
+        />
+        <button className="btn btn-primary" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+
 
     </nav>
   );
